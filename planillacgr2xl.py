5887a50380a1abbd10bda4279195d2da34171c14
rtribaldos <rtribaldos@registro-publico.gob.pa>
2015-07-21 16:20:23 -0500

    feat: se establecen títulos y anchos para todas las columnas a procesar

--- planillacgr2xl.py
+++ planillacgr2xl.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#import Tkinter, Tkconstants, tkFileDialog, tkMessageBox, os.path, re, time
-#from mods import bnp, rth
-
-# encoding=utf8
 import sys, os
 from mods import rth_helper_fns as rth, rth_helper_cls as rthCls, Reporte
 
+# encoding=utf8
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -70,19 +67,80 @@ class CgrDoc:
     def setColumnas(self):
         if self.tipo == 'salarios':
             self.columnas = [
-                ('Código', 7),
+                ('Numero de Cheque/Recibo', 7),
                 ('Nombre', 20),
-                ('1er apellido', 15),
-                ('2do apellido', 15),
-                ('Apellido de casada', 15),
-                ('Cédula', 13),
-                ('Otros', 1000)
+                ('Apellido Paterno', 15),
+                ('Apellido Materno', 15),
+                ('Apellido de Casada', 15),
+                ('Provincia de Cedula', 2),
+                ('Iniciales de la Cedula', 2),
+                ('Tomo de la Cedula', 4),
+                ('Asiento de la Cedula', 5),
+                ('Numero de Seguro Social', 7),
+                ('Codigo de Area', 1),
+                ('Codigo de Entidad', 2),
+                ('Numero de Planilla', 5),
+                ('Numero de Posición', 5),
+                ('Estatus del Funcionario', 2),
+                ('Monto de Sueldo Bruto', 10),
+                ('Monto de Seguro Social', 8),
+                ('Clave de Renta', 3),
+                ('Monto de Impuesto sobre la Renta', 8),
+                ('Monto de Seguro Educativo', 8),
+                ('Monto de Descuento-1', 8),
+                ('Clave de Descuento-1 ', 1),
+                ('Monto de Descuento-2', 8),
+                ('Clave Descuento-2 ', 1),
+                ('Monto de Descuento-3', 8),
+                ('Clave de Descuento-3 ', 1),
+                ('Monto de Descuento-4', 8),
+                ('Clave de Descuento-4 ', 1),
+                ('Monto de Descuento-5', 8),
+                ('Clave de Descuento-5 ', 1),
+                ('Monto de Descuento-6', 8),
+                ('Clave de Descuento-6 ', 1),
+                ('Monto de Descuento-7', 8),
+                ('Clave de Descuento-7 ', 1),
+                ('Monto de Sueldo Neto', 9),
+                ('Fecha de Pago', 8),
+                ('Tipo de Pago', 1),
+                ('Forma de Pago', 2),
+                ('Codigo de Banco', 9),
+                ('Tipo de Cuenta', 1),
+                ('Numero de Cuenta', 20),
+                ('Centro de Pago', 10),
+                ('Provincia donde Labora', 2),
+                ('Año de Proceso', 4),
+                ('Mes de Proceso', 1),
+                ('Quincena de Proceso ', 1)
             ]
 
         elif self.tipo == 'descuentos':
             self.columnas =  [
-                ('Cédula', 13),
-                ('Otros', 1000)
+                ('Provincia de Cedula', 2),
+                ('Iniciales de la Cedula', 2),
+                ('Tomo de la Cedula', 4),
+                ('Asiento de la Cedula', 5),
+                ('Tipo de descuento', 1),
+                ('Fecha de Aplicación', 8),
+                ('Código de Aplicación', 1),
+                ('Área', 1),
+                ('Entidad', 2),
+                ('Número de Planilla', 5),
+                ('Número de Posición', 5),
+                ('Estado del Empleado', 2),
+                ('Clave del Descuento', 8),
+                ('Subclave del Descuento', 8),
+                ('Posición del Descuento', 2),
+                ('Monto Inicial', 8),
+                ('Valor  A Efectuar', 7),
+                ('Valor Efectuado', 7),
+                ('Saldo del Descuento', 7),
+                ('Demandante', 40),
+                ('Código del Préstamo', 1),
+                ('Nombre', 20),
+                ('Apellido', 15),
+                ('Blanco', 1)
             ]
 
     def getColKeys(self):
@@ -141,7 +199,7 @@ try:
 
 except NameError:
     e = sys.exc_info()[0]
-    print u'Upps.. ocurrió un error:', e
+    print u'¡Upps.. ocurrió un error!\n\nPor favor, intente nuevamente.', e
 
 finally:
     print '\n\nPresione cualquier tecla para salir...'
