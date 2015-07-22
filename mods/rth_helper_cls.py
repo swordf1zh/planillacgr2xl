0328af7b4dc009f67168d10aab000dcf8e3ec893
swordf1zh <trblds@gmail.com>
2015-07-22 10:42:31 -0500

    feat(TxtTitleBox):  clase para desplegar títulos enmarcados en formato de texto plano

--- mods/rth_helper_cls.py
+++ mods/rth_helper_cls.py
@@ -3,6 +3,7 @@
 import os
 
 class Filename:
+
     def __init__(self, name):
         self.filename = os.path.abspath(name)
         if self.filename:
@@ -10,3 +11,57 @@ class Filename:
             self.name, self.extension = os.path.splitext(self.filext)
             self.isFile = os.path.isfile(name)
             self.isDir = os.path.isdir(name)
+
+
+
+class TxtTitleBox:
+
+    def __init__(self, titulo):
+        self.width = len(titulo)
+        self.boxContents = []
+        self.addTitulo(titulo)
+
+
+    def vborder(self):
+        border = '-' * (self.width + 2)
+        return '*%s*' % border
+
+
+    def padText(self, text, pos='c'):
+        if pos == 'c': padded = text.center(self.width)
+        elif pos == 'l': padded = text.ljust(self.width)
+        elif pos == 'r': padded = text.rjust(self.width)
+        return ' %s ' % padded
+
+
+    def addTitulo(self, titulo):
+        self.boxContents.append('|%s|' % self.padText(titulo.upper()))
+        self.boxContents.append('|%s|' % self.padText('-' * len(titulo)))
+
+
+    def addEspacio(self):
+        return '|%s|' % self.padText('')
+
+
+    def addLinea(self, linea='', pos='c'):
+        if isinstance(linea, list):
+            for l in linea:
+                self.boxContents.append('|%s|' % self.padText(l, pos))
+        else:
+            self.boxContents.append('|%s|' % self.padText(linea, pos))
+
+
+    def makeBox(self):
+        box = []
+        box.append(self.vborder())
+        box.append(self.addEspacio())
+        for linea in self.boxContents:
+            box.append(linea)
+        box.append(self.addEspacio())
+        box.append(self.vborder())
+        return box
+
+
+    def getBox(self):
+        box = self.makeBox()
+        return '\n'.join(box)
