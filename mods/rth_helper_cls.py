0bc47a99ee889c851490654a0649e3510f948f89
rtribaldos <rtribaldos@registro-publico.gob.pa>
2016-10-10 12:20:49 -0500

    feat: nueva clase 'MyException'

--- mods/rth_helper_cls.py
+++ mods/rth_helper_cls.py
@@ -10,3 +10,9 @@ class Filename:
             self.name, self.extension = os.path.splitext(self.filext)
             self.isFile = os.path.isfile(name)
             self.isDir = os.path.isdir(name)
+
+
+class MyException(Exception):
+    def __init___(self,dErrorArguments):
+        Exception.__init__(self,"my exception was raised with arguments {0}".format(dErrorArguments))
+        self.dErrorArguments = dErrorArguments
